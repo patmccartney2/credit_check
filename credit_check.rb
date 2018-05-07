1e5ebfac6961877ac61734a17da61a62824bd52d
patmccartney2 <30877651+patmccartney2@users.noreply.github.com>
2018-05-07 00:11:47 -0600

    Add files via upload
    updated with a better attempt made.  Still not sure how to get it working, only 75% understand what I'm writing.

--- credit_check.rb
+++ credit_check.rb
@@ -3,17 +3,26 @@ card_number = "4929735477250543"
 valid = false
 
 def cc_check(card_number)
-  ccnumber = card_number.to_s
-  reccnumber = ccnumber.reverse
-  int = reccnumber.to_i
-  array = int.digits
-  revarray = array.reverse
-  summed = revarray.at(1, 2) * 2
-puts summed
-
-  #I got stuck trying to get the everyother position in the array to double.  I scoured ruby docs and looked up other people's
-  #projects working on this algorithmn in Ruby and could not make heads or tales of how their code managed to this.
-
+  ccstr = card_number.to_s.split('')
+  ccarray = []
+  ccarray << ccstr
+  ccarray.each_with_index.map { |x,y|
+    if (y % 2 != 0)
+      x = x*2
+    end
+  }
+  ccarray.map {|x|
+  if (x > 9)
+    x = x-9
+  else
+    x
+  end
+  }
+  if ccarray.sum % 10 == 0
+    p "valid"
+  else
+    p "invalid"
+  end
 
 end
 
